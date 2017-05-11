389fe82b519b36b836e510ffb8fd9b1db2ca5425
David B <dbobsb@hotmail.com>
2017-05-10 17:23:20 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//Group Name: ???l337fac3less
+//Members: David Branson, Dennis Lang, Jesse Mendoza
+//Course: CIS-17C Data Structures
+//Professor: John W O'connor
+//Date Started: 10, May 2017
+//Date Finished: ???
+//
+//Final Project
+//Description:
+//Plagiarism Tool (Page 1099 Getting Started with gaddis #11)
+//1. Write a program that opens two text files and reads their contents into two separate queues. (#3 Save Files, #1 Queue, #2 Dynamic input)
+//2. The program should then determine whether the files are identical by comparing the characters in the queues.
+//3. When two nonidentical characters are encountered, the program should display a message indicating that the les are not the same.
+//4. If both queues contain the same set of characters, a message should be displayed indicating that the les are identical.
+//
+//Milestones
+//1. Week 1: Working Prototype
+//2. Week 2: 
+//3. Week 3:
+//4. Week 4: Final Program, hits all targets, and QA tested. 
+
